023038f9897bd0fab865996cfe379cee19f155c5
vanyajet <vanyajet@gmail.com>
2021-06-17 02:22:30 +0400

    come on

--- nonlinear_dj/settings.py
+++ nonlinear_dj/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SITE_ID = 1
--- nonlinear_dj/urls.py
+++ nonlinear_dj/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('*', TemplateView.as_view(template_name='index.html')),
     path('api/', include('mainapp.api.urls'))
 ]
     # path('', index),
