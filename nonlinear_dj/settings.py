bc9dbfcc969ccab12e325e02bb79e90bf3ff6858
vanyajet <vanyajet@gmail.com>
2021-06-17 01:35:35 +0400

    come on

--- nonlinear_dj/settings.py
+++ nonlinear_dj/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SITE_ID = 1
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'nonlinear/build/static'),
