2e6735bdc15f8218a763660bbeaaf06f36578ba6
vanyajet <vanyajet@gmail.com>
2021-06-16 18:49:52 +0400

    req and proc

--- nonlinear_dj/settings.py
+++ nonlinear_dj/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'nonlinear/build/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'nonlinear/build/static'),
+# ]
