9123944d0549daa1760c3e311f7b845ff67a5887
vanyajet <vanyajet@gmail.com>
2021-06-17 11:58:53 +0400

    deploy

--- nonlinear_dj/settings.py
+++ nonlinear_dj/settings.py
@@ -119,12 +119,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
